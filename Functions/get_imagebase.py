a066f7f135cdf3d82b878cae585d4513d620b2b2
nicole <nicole@intezer.com>
2024-11-03 21:44:41 -0600

    Added support for imagebase and segment based on name

---
+++ Functions/get_imagebase.py
@@ -0,0 +1,36 @@
+"""
+    FeatureProof Middleware function: <function_name>.py
+
+    <Brief description of the function>
+
+    :param <param1>: <Description of param1>
+    :param <param2>: <Description of param2>
+    :return: <Description of the return value>
+"""
+import os
+filename = os.path.splitext(os.path.basename(__file__))[0][:-3]
+logger = fp.logger
+
+def function_6(): # Add parameters as needed
+    logger.debug(f"{filename} for IDA Pro 6.8-7.x called successfully!")
+    # Implementation for IDA Pro 6.8-7.x
+
+def function_8(): # Add parameters as needed
+    logger.debug(f"{filename} for IDA Pro 7.x-8.4 called successfully!")
+    return idaapi.get_imagebase()
+    # Implementation for IDA Pro 7.x-8.4
+
+def function_9(): # Add parameters as needed
+    logger.debug(f"{filename} for IDA Pro 9+ called successfully!")
+    # Implementation for IDA Pro 9+
+
+def get_function():
+    logger.debug(f"{filename} hotloaded successfully!")
+    return {
+        8: {
+            'implementation': function_8,
+            'description': '<Brief description of the function>',
+            'parameters': ['<param1>', '<param2>'],
+            'return_type': '<Return type>'
+        },
+    }
