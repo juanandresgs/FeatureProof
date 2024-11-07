5c8525f2a3c4e4dc1320e57cb8fa102937130698
JAGS <jags.sec@gmail.com>
2024-11-07 16:19:52 -0500

    Added debug info to get_all_function_address_by_type.py

--- Functions/get_all_function_addresses_by_type.py
+++ Functions/get_all_function_addresses_by_type.py
@@ -18,8 +18,11 @@ def function_8(func_type):
     logger.debug(f"{filename} for IDA Pro 7.x-8.4 called successfully!")
     functions = []
     for func_ea in fp.walk_functions():
+        logger.debug(f"Function: {fp.format_address(func_ea)}")
         if idaapi.get_func(func_ea).flags & func_type: #TODO: Should this be abstracted independently?
+            logger.debug(f"  Function of type {func_type} found: {fp.format_address(func_ea)}")
             functions.append(fp.format_address(func_ea))
+    logger.debug(f"Found {len(functions)} functions of type {func_type}")
     return functions
 
 def function_9():
